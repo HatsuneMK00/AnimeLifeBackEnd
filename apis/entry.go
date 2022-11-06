d6af12c4497f88a05bb8e5199ef2eeafec682678
makise <10175101106@stu.ecnu.edu.cn>
2022-11-07 02:18:22 +0800

    framework: remove unused login api and service files

--- apis/entry.go
+++ apis/entry.go
@@ -3,19 +3,16 @@ package apis
 import "AnimeLifeBackEnd/services"
 
 type ApiGroup struct {
-	Login          LoginApi
 	User           UserApi
 	AnimeRecordApi AnimeRecordApi
 }
 
 var (
-	loginService       = services.ServiceGroupApp.Login
 	userService        = services.ServiceGroupApp.User
 	animeRecordService = services.ServiceGroupApp.AnimeRecord
 )
 
 var ApiGroupApp = ApiGroup{
-	Login:          loginApi{},
 	User:           userApi{},
 	AnimeRecordApi: animeRecordApi{},
 }
--- services/entry.go
+++ services/entry.go
@@ -2,12 +2,10 @@ package services
 
 type ServiceGroup struct {
 	User        UserService
-	Login       LoginService
 	AnimeRecord AnimeRecordService
 }
 
 var ServiceGroupApp = ServiceGroup{
 	User:        userService{},
-	Login:       loginService{},
 	AnimeRecord: animeRecordService{},
 }
